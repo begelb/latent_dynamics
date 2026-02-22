97e3d28e39e03a2dbec30d3ab6883e2245db117f
begelb <69121050+begelb@users.noreply.github.com>
2026-02-22 15:52:28 -0500

    Update morse_graph for 1D case

--- morse_graph.py
+++ morse_graph.py
@@ -23,21 +23,22 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_dir',help='Directory of config files',type=str,default='config/')
-    parser.add_argument('--config',help='Config file inside config_dir',type=str,default='arctan.txt')
-    parser.add_argument('--init',help='Initial subdivisions',type=int,default=0)
-    parser.add_argument('--smin',help='Min subdivisions',type=int,default=10)
-    parser.add_argument('--smax',help='Min subdivisions',type=int,default=12)
+    parser.add_argument('--config',help='Config file inside config_dir',type=str,default='coral.txt')
+    parser.add_argument('--init',help='Initial subdivisions',type=int,default=6)
+    parser.add_argument('--smin',help='Min subdivisions',type=int,default=8)
+    parser.add_argument('--smax',help='Max subdivisions',type=int,default=10)
+    parser.add_argument('--lower_dim',help='Dimension of latent space',type=int,default=1)
 
     args = parser.parse_args()
     subdiv_min = args.smin
     subdiv_max = args.smax
     subdiv_init = args.init
+    lower_dim = args.lower_dim
 
     config_fname = args.config_dir + args.config
 
     config = Config(config_fname)
     
-    # num_pts = config.num_pts
     ex_index = config.ex_index
     output_dir = config.output_dir
 
@@ -54,8 +55,8 @@ if __name__ == "__main__":
     encoder.eval()
 
     base_data_dir = config.data_dir
-    train_data_path = os.path.join(base_data_dir, '2train.csv')
-    test_data_path = os.path.join(base_data_dir, '2test.csv')
+    train_data_path = os.path.join(base_data_dir, 'train.csv')
+    test_data_path = os.path.join(base_data_dir, 'test.csv')
     train_data = np.loadtxt(train_data_path, delimiter=',', skiprows=1)
     test_data = np.loadtxt(test_data_path, delimiter=',', skiprows=1)
 
@@ -77,37 +78,56 @@ if __name__ == "__main__":
 
     all_data = np.vstack((x_train_scaled, x_test_scaled, y_train_scaled, y_test_scaled))
 
-
     all_data_tensor = torch.as_tensor(all_data, dtype=torch.float32, device=device)
     with torch.no_grad():
         all_data_latent = encoder(all_data_tensor).cpu().numpy()
     lower_bounds_x = np.min(all_data_latent[:,0])
     upper_bounds_x = np.max(all_data_latent[:,0])
-    lower_bounds_y = np.min(all_data_latent[:,1])
-    upper_bounds_y = np.max(all_data_latent[:,1])
+
+    if lower_dim == 2:
+        lower_bounds_y = np.min(all_data_latent[:,1])
+        upper_bounds_y = np.max(all_data_latent[:,1])
     
     g = partial(g_base, dynamics_model=dynamics_model, device=device)
 
     def G(rect):
         return CMGDB.BoxMap(g, rect, padding=True)
 
-    lower_bounds = [lower_bounds_x, lower_bounds_y]
-    upper_bounds = [upper_bounds_x, upper_bounds_y]
+    if lower_dim == 1:
+        lower_bounds = [lower_bounds_x]
+        upper_bounds = [upper_bounds_x]
+    elif lower_dim == 2:
+        lower_bounds = [lower_bounds_x, lower_bounds_y]
+        upper_bounds = [upper_bounds_x, upper_bounds_y]
 
     subdiv_limit = config.subdiv_limit
 
     model = CMGDB.Model(subdiv_min, subdiv_max, subdiv_init, subdiv_limit, lower_bounds, upper_bounds, G)
 
-    morse_graph, map_graph = CMGDB.ComputeConleyMorseGraph(model)
+    morse_graph, map_graph = CMGDB.ComputeMorseGraph(model)
     
     MG_dir = os.path.join(output_dir, 'MG')
 
-    morse_graph_plot = CMGDB.PlotMorseGraph(morse_graph)
-    morse_graph_plot.render(os.path.join(MG_dir, 'morse_graph'), format='pdf', view=False, cleanup=False)
-    morse_graph_plot.render(os.path.join(MG_dir, 'morse_graph'), format='png', view=False, cleanup=True)
+    color_list = [ 
+                '#FFB000',  # Gold 
+                '#DC267F',  # Magenta
+                '#648FFF', # Blue
+                '#FE6100',  # Orange
+                '#785EF0',  # Purple
+                ]
 
-    morse_sets_plot = CMGDB.PlotMorseSets(morse_graph, xlim=[lower_bounds[0], upper_bounds[0]], ylim=[lower_bounds[1], upper_bounds[1]], xlabel='$x_1$', ylabel='$x_2$', fontsize=20, fig_fname=os.path.join(MG_dir, 'morse_sets.pdf'))
-    morse_sets_plot = CMGDB.PlotMorseSets(morse_graph, xlim=[lower_bounds[0], upper_bounds[0]], ylim=[lower_bounds[1], upper_bounds[1]], xlabel='$x_1$', ylabel='$x_2$', fontsize=20, fig_fname=os.path.join(MG_dir, 'morse_sets.png'))
+    morse_graph_plot = CMGDB.PlotMorseGraph(morse_graph, clist=color_list)
+
+    morse_graph_plot.render(os.path.join(MG_dir, 'morse_graph'), format='pdf', view=False, cleanup=False)
+    morse_graph_plot.render(os.path.join(MG_dir, 'morse_graph'), format='png', view=False, cleanup=False)
+
+    if lower_dim == 1:
+        morse_sets_plot = CMGDB.PlotMorseSets(morse_graph, clist=color_list, fontsize=20, fig_fname=os.path.join(MG_dir, 'morse_sets2.pdf'))
+        morse_sets_plot = CMGDB.PlotMorseSets(morse_graph, clist=color_list, fontsize=20, fig_fname=os.path.join(MG_dir, 'morse_sets2.png'))
+
+    elif lower_dim == 2:
+        morse_sets_plot = CMGDB.PlotMorseSets(morse_graph, xlim=[lower_bounds[0], upper_bounds[0]], ylim=[lower_bounds[1], upper_bounds[1]], xlabel='$x_1$', ylabel='$x_2$', fontsize=20, fig_fname=os.path.join(MG_dir, 'morse_sets.pdf'))
+        morse_sets_plot = CMGDB.PlotMorseSets(morse_graph, xlim=[lower_bounds[0], upper_bounds[0]], ylim=[lower_bounds[1], upper_bounds[1]], xlabel='$x_1$', ylabel='$x_2$', fontsize=20, fig_fname=os.path.join(MG_dir, 'morse_sets.png'))
 
     CMGDB.SaveMorseSets(morse_graph, os.path.join(MG_dir, 'morse_sets'))
     end_time = time.perf_counter()
